eb693d07eaa1b3d6432b3194799348c4ce5e5b8f
s.soni <s.soni@workday.com>
2024-02-18 19:17:58 +0000

    Add Greyscale trigger button and image

--- app/src/main/java/com/example/imageai/MainActivity.kt
+++ app/src/main/java/com/example/imageai/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.imageai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity() , View.OnClickListener{
+    lateinit var greyScaleButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        greyScaleButton = findViewById(R.id.blackAndWhite)
+        greyScaleButton.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this,"Clicked on the grey scale triggered button!",Toast.LENGTH_LONG).show()
+
+    }
+
 }
+
