3b067a05586f7e6567208957ac4574bbf3898b77
s.soni <s.soni@workday.com>
2024-02-19 20:51:43 +0000

    Reverse filter

--- app/src/main/java/com/example/imageai/MainActivity.kt
+++ app/src/main/java/com/example/imageai/MainActivity.kt
@@ -1,31 +1,38 @@
 package com.example.imageai
 
-import android.graphics.ColorFilter
 import android.graphics.ColorMatrix
 import android.graphics.ColorMatrixColorFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var greyScaleButton: Button
+    lateinit var originalImageView: ImageView
+    var isFilterApplied: Boolean = false
 
-class MainActivity : AppCompatActivity() , View.OnClickListener{
-    lateinit var greyScaleButton : Button
-    lateinit var image : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         greyScaleButton = findViewById(R.id.blackAndWhite)
+        originalImageView = findViewById(R.id.imageView2)
         greyScaleButton.setOnClickListener(this)
-
     }
 
     override fun onClick(v: View?) {
-        image = findViewById(R.id.imageView2)
-        image.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f)})
-        Toast.makeText(this,"Clicked on the grey scale triggered button!",Toast.LENGTH_LONG).show()
+        if (!isFilterApplied) {
+            originalImageView.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f) })
+            Toast.makeText(this, "Filter applied.", Toast.LENGTH_LONG).show()
+            isFilterApplied = true
+        }
+        else {
+            originalImageView.clearColorFilter()
+            Toast.makeText(this, "Filter removed.", Toast.LENGTH_LONG).show()
+            isFilterApplied = false
+        }
     }
 
 }
