5425a877c912c311307e1f4031d6d3ae44d95eba
s.soni <s.soni@workday.com>
2024-02-19 19:50:05 +0000

    Black and White filter

--- app/src/main/java/com/example/imageai/MainActivity.kt
+++ app/src/main/java/com/example/imageai/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.imageai
 
+import android.graphics.ColorFilter
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() , View.OnClickListener{
     lateinit var greyScaleButton : Button
+    lateinit var image : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,8 +23,9 @@ class MainActivity : AppCompatActivity() , View.OnClickListener{
     }
 
     override fun onClick(v: View?) {
+        image = findViewById(R.id.imageView2)
+        image.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f)})
         Toast.makeText(this,"Clicked on the grey scale triggered button!",Toast.LENGTH_LONG).show()
-
     }
 
 }
