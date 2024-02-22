72820fff4df6d451e2fc35fdcfcda1c9cb6a7183
Siddharth Soni <siddharthsoni993@gmail.com>
2024-02-22 20:02:54 +0000

    Build home page
    * home page logo
    
    * home page
    
    * Home page integration with photo editor
    
    * home page
    
    * resolve comflicts
    
    * Remove new lines
    
    ---------
    
    Co-authored-by: Shananu Soni <shantanu.soni.che13@itbhu.ac.in>
    Co-authored-by: s.soni <s.soni@workday.com>

--- app/src/main/java/com/example/imageai/MainActivity.kt
+++ app/src/main/java/com/example/imageai/MainActivity.kt
@@ -1,39 +1,24 @@
 package com.example.imageai
-
-import android.graphics.ColorMatrix
-import android.graphics.ColorMatrixColorFilter
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-    lateinit var greyScaleButton: Button
-    lateinit var originalImageView: ImageView
-    var isFilterApplied: Boolean = false
+
+class MainActivity : AppCompatActivity(){
+    lateinit var photoEditor: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        greyScaleButton = findViewById(R.id.blackAndWhite)
-        originalImageView = findViewById(R.id.imageView2)
-        greyScaleButton.setOnClickListener(this)
-    }
+        setContentView(R.layout.home_page)
+        photoEditor = findViewById(R.id.photoEdit)
 
-    override fun onClick(v: View?) {
-        if (!isFilterApplied) {
-            originalImageView.colorFilter = ColorMatrixColorFilter(ColorMatrix().apply { setSaturation(0f) })
-            Toast.makeText(this, "Filter applied.", Toast.LENGTH_LONG).show()
-            isFilterApplied = true
-        }
-        else {
-            originalImageView.clearColorFilter()
-            Toast.makeText(this, "Filter removed.", Toast.LENGTH_LONG).show()
-            isFilterApplied = false
-        }
-    }
 
-}
+        photoEditor.setOnClickListener {
+            val intent = Intent(this, PhotoEditor::class.java)
+            startActivity(intent)
+    }
+}}
+
+
 
